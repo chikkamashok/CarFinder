02494c3dc0f0dab096b42e9158f7d131f347eb59
chikkamashok <71232684+chikkamashok@users.noreply.github.com>
2021-09-07 19:45:31 -0400

    Load the tableview with the available car listings.

--- CarFinder/CarFinder/View/CarListScreen.swift
+++ CarFinder/CarFinder/View/CarListScreen.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class CarListScreen: UITableViewController {
 
+    private var viewModel = CarListViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        loadCarListings()
     }
-
-
+    
+    private func loadCarListings() {
+        viewModel.fetchCarListings { [weak self] in
+            self?.tableView.reloadData()
+        }
+    }
 }
 
+// MARK: - TableView
+extension CarListScreen {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.numberOfRowsInSection(section: section)
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CarCell", for: indexPath) as! CarCell
+        
+        let carListing = viewModel.cellForRowAt(indexPath: indexPath)
+        cell.updateCellWith(carListing)
+        return cell
+    }
+}
