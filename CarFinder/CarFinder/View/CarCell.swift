02f8cef8e6b64a95f7150a6baba18c4a4e943335
chikkamashok <71232684+chikkamashok@users.noreply.github.com>
2021-09-07 19:44:38 -0400

    Add UpdateCell method to update the cell with all the data it needs

--- CarFinder/CarFinder/View/CarCell.swift
+++ CarFinder/CarFinder/View/CarCell.swift
@@ -9,12 +9,46 @@ import UIKit
 
 class CarCell: UITableViewCell {
     
+    // MARK: IBOutlets
     @IBOutlet weak var carImageView: UIImageView!
     @IBOutlet weak var carModelDetails: UILabel!
     @IBOutlet weak var carPriceDetails: UILabel!
-    
     @IBOutlet weak var carDealerContact: UIButton!
     
+    // MARK: Methods
+    func updateCellWith(_ listing: CarListing) {
+        
+        self.carModelDetails.text = listing.make
+        self.carPriceDetails.text = "\(listing.currentPrice)"
+        self.carDealerContact.setTitle(listing.dealer?.phone, for: .normal)
+        guard let urlString = listing.images?.small.first, let imageUrl = URL(string: urlString) else {
+            self.carImageView.image = UIImage(named: "noImageAvailable")
+            return
+        }
+        self.carImageView.image = nil
+        getImageDataFrom(url: imageUrl)
+    }
     
-    
+    // MARK: - Get image data
+    private func getImageDataFrom(url: URL) {
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            // Handle Error
+            if let error = error {
+                print("DataTask error: \(error.localizedDescription)")
+                return
+            }
+            
+            guard let data = data else {
+                // Handle Empty Data
+                print("Empty Data")
+                return
+            }
+            
+            DispatchQueue.main.async {
+                if let image = UIImage(data: data) {
+                    self.carImageView.image = image
+                }
+            }
+        }.resume()
+    }
 }
