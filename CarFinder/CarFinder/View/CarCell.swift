6298d00151872947cb8fb0a951627e612d80bf86
chikkamashok <71232684+chikkamashok@users.noreply.github.com>
2021-09-07 20:56:00 -0400

    Update Car model and price details

--- CarFinder/CarFinder/View/CarCell.swift
+++ CarFinder/CarFinder/View/CarCell.swift
@@ -18,20 +18,21 @@ class CarCell: UITableViewCell {
     // MARK: Methods
     func updateCellWith(_ listing: CarListing) {
         
-        self.carModelDetails.text = listing.make
-        self.carPriceDetails.text = "\(listing.currentPrice)"
+        self.carModelDetails.text = getCarModelDetails(listing)
+        self.carPriceDetails.text = getCarPriceDetails(listing)
         self.carDealerContact.setTitle(listing.dealer?.phone, for: .normal)
         guard let urlString = listing.images?.small.first, let imageUrl = URL(string: urlString) else {
             self.carImageView.image = UIImage(named: "noImageAvailable")
             return
         }
-        self.carImageView.image = nil
+//        self.carImageView.image = nil
         getImageDataFrom(url: imageUrl)
     }
     
     // MARK: - Get image data
     private func getImageDataFrom(url: URL) {
         URLSession.shared.dataTask(with: url) { (data, response, error) in
+           
             // Handle Error
             if let error = error {
                 print("DataTask error: \(error.localizedDescription)")
@@ -45,10 +46,33 @@ class CarCell: UITableViewCell {
             }
             
             DispatchQueue.main.async {
-                if let image = UIImage(data: data) {
+                if let image = UIImage(data: data), image.size.height > 10.0 {
+                    print(image.size.height)
                     self.carImageView.image = image
                 }
             }
         }.resume()
+    }
+}
+
+extension CarCell {
+    
+    // MARK: Helper methods
+    private func getCarModelDetails(_ listing: CarListing) -> String {
+        
+        let year = (listing.year != nil) ? String(listing.year!) : ""
+        let make = listing.make ?? ""
+        let model = listing.model ?? ""
+        return year + " " + make + " " + model
+    }
+    
+    private func getCarPriceDetails(_ listing: CarListing) -> String {
+        
+        let price = "$" + ((listing.currentPrice != nil) ? String(Int(listing.currentPrice!)) : "")
+        let mileage = (listing.mileage != nil) ? String(listing.mileage!) : ""
+        let city = listing.dealer?.city ?? ""
+        let state = listing.dealer?.state ?? ""
+        let location = city + ", " + state
+        return price + " | " + mileage + " | " + location
     }
 }
